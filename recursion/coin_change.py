b673421b68f2497c4c6e88ab7e8287c93122c485
Daniel <esc.daniel91@gmail.com>
2021-10-10 14:19:29 -0400

    coin change

--- recursion/coin_change.py
+++ recursion/coin_change.py
@@ -1,6 +1,23 @@
 # given a target amount n and a list of distinct coin values, 
 # whats the fewest coins needed to make the change amount
-def coin(target, coins):
-    pass
+def coin_rec(target, coins):
+    
+    min_coins = target
 
-coin(10,[1,5]) #=> 2
+    if target in coins:
+        return 1
+    
+    else:
+        # for every coin value that is <= my target value
+        for i in coins:
+            if i <= target:
+                # add a coin count + recursive call
+                num_coins = 1 + coin_rec(target-i, coins)
+            if num_coins < min_coins:
+                min_coins = num_coins
+            
+
+    return min_coins
+    
+
+print(coin_rec(13,[1,5, 10])) #=> 1
